dd9258339e10b16acd8ab88c4223ae8dc2c1a190
Hayden <hchalin98@gmail.com>
2023-03-23 19:57:05 -0400

    fixed firebase configuration

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <p className="text-red-500 h-screen  overflow-auto ">This is my app </p>
+    </main>
+  );
+}
