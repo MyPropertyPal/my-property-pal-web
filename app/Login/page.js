9d83f518498f97cb48508468389a3265370d29e0
Hayden <hchalin98@gmail.com>
2023-03-24 15:30:30 -0400

    git error fix attempt

--- app/Login/page.js
+++ app/Login/page.js
@@ -8,73 +8,80 @@ function Login() {
     let email = e.target.email.value;
     let password = e.target.password.value;
 
-
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
 
-        alert(user.email)
+        alert(user.email);
         // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        alert(errorMessage)
+        alert(errorMessage);
       });
   };
 
   return (
-    <div class="w-full max-w-xs m-auto">
+    <div className="w-full max-w-xs m-auto">
       <form
-        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
         onSubmit={handleLogin}
       >
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             for="username"
           >
             Email
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             type="text"
             placeholder="Email"
           />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             for="password"
           >
             Password
           </label>
           <input
-            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
           />
-          <p class="text-red-500 text-xs italic">Please choose a password.</p>
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
         </div>
-        <div class="flex items-center justify-between">
+        <div className="flex items-center justify-between">
           <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             // type="button"
           >
             Sign In
           </button>
           <a
-            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
             href="#"
           >
             Forgot Password?
           </a>
         </div>
+        <div className="flex space-x-9 mt-2">
+          <p className=" text-sm text-gray-500">Dont have an account?</p>
+          <a href="/Signup" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+            Click Here
+          </a>
+        </div>
       </form>
-      <p class="text-center text-gray-500 text-xs">
+      <p className="text-center text-gray-500 text-xs">
         &copy;2020 Acme Corp. All rights reserved.
       </p>
     </div>
---
+++ app/Signup/page.js
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>Wooo!  Sign up!</div>
+  )
+}
+
+export default SignUp
