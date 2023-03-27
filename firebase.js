b0d4bac1615ac1f9ce3892dd92e7dabb0df413e4
Hayden <hchalin98@gmail.com>
2023-03-27 12:03:39 -0400

    revert back to hard coded api keys due to errors

--- firebase.js
+++ firebase.js
@@ -8,14 +8,24 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: `${process.env.FIREBASE_API_KEY}`,
+//   authDomain: `${process.env.FIRBASE_AUTH_DOMAIN}`,
+//   projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+//   storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+//   appId: `${process.env.FIREBASE_APP_ID}`,
+//   measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIRBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDLOemS_I1VO-2FztJW2Tiqz81lKjms7ag",
+  authDomain: "my-property-pal-337e2.firebaseapp.com",
+  projectId: "my-property-pal-337e2",
+  storageBucket: "my-property-pal-337e2.appspot.com",
+  messagingSenderId: "735822899863",
+  appId: "1:735822899863:web:35f0f70698a74349ed17d2",
+  measurementId: "G-JF26VZSKL3",
 };
 
 // Initialize Firebase
