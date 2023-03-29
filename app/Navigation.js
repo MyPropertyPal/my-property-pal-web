5d058e9c9be72d5f32c76c2e5efd678c51dc67b6
Hayden <hchalin98@gmail.com>
2023-03-29 16:34:57 -0400

    renders 2 navbars based on user status

--- app/Navigation.js
+++ app/Navigation.js
@@ -3,12 +3,13 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { getAuth } from "firebase/auth";
-import { signOut } from "firebase/auth";
 import app from "./firebase/config";
+import { useAuthContext } from "./context/AuthContext";
 
 const auth = getAuth(app);
 
 function Header() {
+  const { user } = useAuthContext();
   const router = useRouter();
   const handleSignOut = async () => {
     try {
@@ -19,26 +20,35 @@ function Header() {
       console.error(err);
     }
   };
+  /**
+   * 2 nav bar layouts
+   * 1st: hero section for users who are not loggeg in
+   * 2nd: for users who are logged in
+   */
+
   return (
+    // not logged in
     <header className="bg-blue-500 flex h-[100px]">
-      {/* left side */}
-      <p className=" flex flex-1 ml-10 mt-8">*logo*</p>
-      {/* right side */}
-      <ul className="flex flex-2  space-x-8 my-8 mr-8">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/Login">Login</Link>
-        </li>
-        <li>
-          {/* make pointer/ or a link?? */}
-          <div onClick={handleSignOut}>Sign Out</div>
-        </li>
-      </ul>
-      {/* add some links */}
+      {!user ? (
+        <>
+          <p className=" flex flex-1 ml-10 mt-8">*logo*</p>
+          {/* right side */}
+
+          <ul className="flex flex-2  space-x-8 my-8 mr-8">
+            <li>
+              <Link href="/">Home</Link>{" "}
+            </li>
+            <li>
+              <Link href="/Login">Login</Link>{" "}
+            </li>
+          </ul>
+        </>
+      ) : (
+        <div onClick={handleSignOut}>Sign Out</div>
+      )}
     </header>
   );
 }
 
 export default Header;
+
