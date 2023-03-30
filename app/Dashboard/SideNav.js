b66b5c8ab96c75453e31d80e0e2a1af065ab8621
Hayden <hchalin98@gmail.com>
2023-03-30 18:03:49 -0400

    added settings page

--- app/Dashboard/SideNav.js
+++ app/Dashboard/SideNav.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Link from "next/link";
 function SideNav() {
   return (
     <div className="h-screen bg-slate-600 max-w-[200px] p-2">
-      This is the side nav
+      <Link href="Dashboard/Settings">settings</Link>
     </div>
   );
 }
