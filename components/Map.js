ee008b7cbae4c5c3d57152f6d429204791cb9fcb
Hayden <hchalin98@gmail.com>
2023-04-05 10:33:17 -0400

    added gastonia as center

--- components/Map.js
+++ components/Map.js
@@ -12,11 +12,19 @@ export default function MapBox() {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/light-v10',
-      center: [15.4542, 18.7322],
-      zoom: 14
-    })
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [-81.187302, 35.262081],
+      zoom: 14,
+    });
   }, []);
 
-  return <div className="map-container" ref={mapContainer} style={{width: 400 ,height: 600}}>map</div>;
+  return (
+    <div
+      className="map-container h-screen"
+      ref={mapContainer}
+      // style={{ width: 400, height: 600 }}
+    >
+      map
+    </div>
+  );
 }
