9e239eb8ae9083093dec7f32f3617cadb4d56748
Hayden <hchalin98@gmail.com>
2023-04-13 16:50:42 -0400

    still working on adding fonts

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,17 @@ import { getAuth } from "firebase/auth";
 import { initFirebase } from "@/app/firebase/config";
 import app from "@/app/firebase/config";
 import { AuthContextProvider } from "./context/AuthContext";
+import { Lato } from "@next/font/google";
 export const metadata = {
   title: "My Property Pal",
   description: "My Property Pal",
 };
+
+const lato = Lato({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-lato",
+});
 
 export default function RootLayout({ children }) {
   const auth = getAuth(app);
@@ -16,7 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head></head>
-      <body className="h-screen  bg-[#f6f6f6] ">
+
+      <body className="h-screen  bg-[#f6f6f6] `{lato.variable}`">
         <AuthContextProvider>
           {/* Include shared UI here e.g. a header or sidebar */}
           <Header />
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'lato': ["Lato"],
-      },
+    
     },
   },
   plugins: [],
