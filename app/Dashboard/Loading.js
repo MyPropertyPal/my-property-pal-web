58ca4b8403be7af92ed7e5d31d833ac25785e49b
Hayden <hchalin98@gmail.com>
2023-04-17 13:27:24 -0400

    push commit

--- app/Dashboard/Loading.js
+++ app/Dashboard/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   // color is just for visual
-  return <div className="h-screen bg-green-600">loading the data</div>;
+  return <div className="h-screen ">loading the data</div>;
 }
 
 export default Loading;
