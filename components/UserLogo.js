ac16ee255f912c5897446556a9095660fa4fee5f
Hayden <hchalin98@gmail.com>
2023-04-20 12:43:11 -0400

    Components now use helper fns to get user data

--- components/UserLogo.js
+++ components/UserLogo.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from "react";
 import { Dropdown, Avatar, Text, Grid, User } from "@nextui-org/react";
 import { useAuthContext } from "@/app/context/AuthContext";
+import getUser from "@/app/firebase/firestore/getData";
 
 export default function UserLogo() {
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
   const { user } = useAuthContext();
   // console.log(user.firstName, "======");
+
+  // call firebase helper fn
+  const getUserInfo = async () => {
+    const docSnap = await getUser("users", user.email);
+    setFirstName(docSnap.firstName);
+    setLastName(docSnap.lastName);
+    console.log(docSnap);
+  };
+
+  useEffect(() => {
+    // get user data once on page load
+    getUserInfo();
+  }, []);
 
   return (
     <Grid.Container>
@@ -15,7 +32,7 @@ export default function UserLogo() {
               as="button"
               size="lg"
               color="primary"
-              name="Tony Reichert"
+              name={`${firstName} ${lastName}`}
               description={user.email}
               src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
             />
