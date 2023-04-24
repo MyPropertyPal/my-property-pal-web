1f340435b1f7ec0b523b959de4e24edb8cf0bd2f
Hayden <hchalin98@gmail.com>
2023-04-24 19:45:26 -0400

    sidenav now has smooth collapse transition

--- app/Dashboard/SideNav.js
+++ app/Dashboard/SideNav.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 function SideNav() {
   return (
-    <div className="flex flex-col space-y-4 bg-slate-600 w-[250px] p-2">
+    <div className="flex flex-col space-y-4 bg-slate-600 w-[80px] hover:w-[200px] transition-all p-2">
       <Link href="/">Home</Link>
       <Link href="Dashboard/Search">Search</Link>
       <Link href="Dashboard/Settings">Settings</Link>
