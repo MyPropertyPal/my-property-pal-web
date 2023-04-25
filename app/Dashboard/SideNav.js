c33f27a718f4d1133968d7ecc6a67f9dd2b85ec7
Hayden <hchalin98@gmail.com>
2023-04-25 18:18:44 -0400

    added ui to sidenav

--- app/Dashboard/SideNav.js
+++ app/Dashboard/SideNav.js
@@ -2,10 +2,34 @@ import React from "react";
 import Link from "next/link";
 function SideNav() {
   return (
-    <div className="flex flex-col space-y-4 bg-slate-600 w-[80px] hover:w-[200px] transition-all p-2">
-      <Link href="/">Home</Link>
-      <Link href="Dashboard/Search">Search</Link>
-      <Link href="Dashboard/Settings">Settings</Link>
+    <div className="flex flex-col space-y-4 bg-slate-600 w-[100px] hover:w-[220px] transition-all p-2">
+      <div className="flex ">
+        <span className="bg-[#DBB073]  hover:visible w-[10px] h-[2px] mr-2 my-auto"></span>
+        <Link
+          className="hover:bg-slate-500 transition-all hover:px-3 w-fit rounded-lg"
+          href="/"
+        >
+          Home
+        </Link>
+      </div>
+      <div className="flex">
+        <span className="bg-[#DBB073]  hover:visible w-[10px] h-[2px] mr-2 my-auto"></span>
+        <Link
+          className="hover:bg-slate-500 transition-all hover:px-3 w-fit rounded-lg"
+          href="Dashboard/Search"
+        >
+          Search
+        </Link>
+      </div>
+      <div className="flex">
+        <span className="bg-[#DBB073]  hover:visible w-[10px] h-[2px] mr-2 my-auto"></span>
+        <Link
+          className="hover:bg-slate-500 transition-all hover:px-3 w-fit rounded-lg"
+          href="Dashboard/Settings"
+        >
+          Settings
+        </Link>
+      </div>
     </div>
   );
 }
