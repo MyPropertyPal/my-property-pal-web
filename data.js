cfc6e49389d4977af547e767cbee0369fc2d4f1b
Hayden <hchalin98@gmail.com>
2023-04-26 17:40:32 -0400

    added fake property data to independent file

---
+++ data.js
@@ -0,0 +1,32 @@
+export const properties = [
+  {
+    id: 1,
+    address: "123 main st.",
+    style: "Single Family",
+    price: 600000,
+  },
+  {
+    id: 2,
+    address: "123 staff st.",
+    style: "Multi Family",
+    price: 3000000,
+  },
+  {
+    id: 3,
+    address: "123 huff st.",
+    style: "Single Family",
+    price: 400000,
+  },
+  {
+    id: 4,
+    address: "123 drake st.",
+    style: "Short Term Rental",
+    price: 450000,
+  },
+  {
+    id: 5,
+    address: "123 blue st.",
+    style: "Comercial",
+    price: 1000000,
+  },
+];
