a50ad781777e63df3dcc543a855adf7e0df61876
Hayden <hchalin98@gmail.com>
2023-06-01 13:43:18 -0400

    added loading screens to the loading of the user login page for both the routing and UI once firebase is loading the user info

---
+++ app/Login/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className="w-full max-w-xs m-auto mt-[250px] shadow-md rounded">
+    <div className="p-9">
+      <h1>New User Information loading</h1>
+    </div>
+  </div>
+  )
+}
+
+export default loading
--- app/Login/page.js
+++ app/Login/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { Loading } from "@nextui-org/react";
 import Link from "next/link";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/config";
@@ -9,9 +10,11 @@ import { useRouter } from "next/navigation";
 function Login() {
   const [passEntered, setPassEntered] = useState(true);
   const [emailEntered, setEmailEntered] = useState(true);
+  const [loading, setIsLoading] = useState(false);
   let router = useRouter();
   const handleLogin = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     let email = e.target.email.value;
     let password = e.target.password.value;
 
@@ -40,85 +43,100 @@ function Login() {
 
     // else successful
     console.log(result);
+    setIsLoading(false);
+
     return router.push("/");
   };
 
   return (
-    <div className="w-full max-w-xs m-auto mt-[250px]">
-      <form
-        className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleLogin}
-      >
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            for="email"
-          >
-            Email
-          </label>
-          <input
-            className={`shadow appearance-none border ${
-              !emailEntered ? "border-red-500" : ""
-            } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-            id="email"
-            type="text"
-            placeholder="Email"
-          />
-          {!emailEntered ? (
-            <p className="text-red-500 text-xs italic">
-              Please enter your Email.
-            </p>
-          ) : null}
+    <>
+      {loading ? (
+        <div className="flex w-full max-w-xs m-auto mt-[250px] shadow-md rounded p-auto h-[320px] ">
+            <Loading className="justify-center mx-auto" />
+          {/* <div className="bg-slate-50  ">
+          </div> */}
         </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
-          >
-            Password
-          </label>
-          <input
-            className={`shadow appearance-none border ${
-              !passEntered ? "border-red-500" : ""
-            } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-          {!passEntered ? (
-            <p className="text-red-500 text-xs italic">
-              Please enter your Password.
-            </p>
-          ) : null}
+      ) : (
+
+
+        <div className="w-full max-w-xs m-auto mt-[250px]">
+          <form
+            className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            onSubmit={handleLogin}
+            >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="email"
+                >
+                Email
+              </label>
+              <input
+                className={`shadow appearance-none border ${
+                  !emailEntered ? "border-red-500" : ""
+                } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                id="email"
+                type="text"
+                placeholder="Email"
+                />
+              {!emailEntered ? (
+                <p className="text-red-500 text-xs italic">
+                  Please enter your Email.
+                </p>
+              ) : null}
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="password"
+                >
+                Password
+              </label>
+              <input
+                className={`shadow appearance-none border ${
+                  !passEntered ? "border-red-500" : ""
+                } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                id="password"
+                type="password"
+                placeholder="******************"
+                />
+              {!passEntered ? (
+                <p className="text-red-500 text-xs italic">
+                  Please enter your Password.
+                </p>
+              ) : null}
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-[#3AB0FF] hover:bg-[#287cb4] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+                >
+                Sign In
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-[#3AB0FF] hover:text-[#287cb4]"
+                href="#"
+                >
+                Forgot Password?
+              </a>
+            </div>
+            <div className="flex space-x-9 mt-2">
+              <p className=" text-sm text-gray-500">Dont have an account?</p>
+              <Link
+                href="/Signup"
+                className="inline-block align-baseline font-bold text-sm text-[#3AB0FF] hover:text-[#287cb4]"
+                >
+                Click Here
+              </Link>
+            </div>
+          </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy;2020 Acme Corp. All rights reserved.
+          </p>
         </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-[#3AB0FF] hover:bg-[#287cb4] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Sign In
-          </button>
-          <a
-            className="inline-block align-baseline font-bold text-sm text-[#3AB0FF] hover:text-[#287cb4]"
-            href="#"
-          >
-            Forgot Password?
-          </a>
-        </div>
-        <div className="flex space-x-9 mt-2">
-          <p className=" text-sm text-gray-500">Dont have an account?</p>
-          <Link
-            href="/Signup"
-            className="inline-block align-baseline font-bold text-sm text-[#3AB0FF] hover:text-[#287cb4]"
-          >
-            Click Here
-          </Link>
-        </div>
-      </form>
-      <p className="text-center text-gray-500 text-xs">
-        &copy;2020 Acme Corp. All rights reserved.
-      </p>
-    </div>
+
+      )}
+    </>
   );
 }
 
